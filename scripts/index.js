a3e3b576d07ff09a9bc3b01a6fc0f86b78205e7b
Viktor Matveev <vikt0r@yandex.ru>
2024-01-12 21:59:38 +0300

    добавлен вывод и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const places = document.querySelector('.places');
+const placesList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+function craeteCard(cardInfoName, cardInfoLink, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__image').src = cardInfoLink;
+  cardElement.querySelector('.card__image').alt = cardInfoName;
+  cardElement.querySelector('.card__title').textContent = cardInfoName;
+
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
+
+function deleteCard(element) {
+  element.closest('.card').remove();
+}
+
+function showCards(cards) {
+  cards.forEach((card) => {
+    const cardElement = craeteCard(card.name, card.link, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+showCards(initialCards);
